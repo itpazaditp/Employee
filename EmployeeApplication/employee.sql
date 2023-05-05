f2d17030de55fb7f677de697979465da02639017
itpazaditp <121937022+itpazaditp@users.noreply.github.com>
2023-05-05 03:10:18 -0700

    test

---
+++ EmployeeApplication/employee.sql
@@ -0,0 +1,10 @@
+create database MachineTest
+create table employee(employee_id int primary key,employee_name varchar(255),employee_address varchar(255))
+
+insert into employee values(1,'Vaibhav','Pune'),
+(2,'Vaibhav','Pune'),
+(3,'Vaibhav','Pune'),
+(4,'Vaibhav','Pune'),
+(5,'Vaibhav','Pune')
+
+select * from employee
